f4505c3a5411fbc5943e343b7c005e54b111cd2e
notnypical <46350569+notnypical@users.noreply.github.com>
2021-01-29 09:14:42 +0100

    Add basic application

---
+++ main_window.h
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2021 LotrioLabs, <https://lotriolabs.github.io>.
+ *
+ * This file is part of Lotrio-Qt.
+ *
+ * Lotrio-Qt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lotrio-Qt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lotrio-Qt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include <QMainWindow>
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+};
+
+#endif // MAIN_WINDOW_H
