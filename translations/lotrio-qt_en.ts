635ce7c88d4d3232f931a6019b8f018d83110b1f
notnypical <46350569+notnypical@users.noreply.github.com>
2021-02-20 23:03:59 +0100

    Update translation template

--- translations/lotrio-qt_en.ts
+++ translations/lotrio-qt_en.ts
@@ -13,7 +13,7 @@
     <name>ColophonAboutPage</name>
     <message>
         <location filename="../colophon_about_page.cpp" line="33"/>
-        <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;%1 is an open source visualization tool written in Qt for C++ and designed for easy creation and graphical representation of lottery data.&lt;/p&gt;&lt;p&gt;Copyright &amp;copy; 2021 &lt;a href=&quot;%2&quot;&gt;%3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;This application is licensed under the terms of the &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.en.html&quot;&gt;GNU General Public License, version 3&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;%1 is an open source visualization tool written in Qt for C++ and designed for easy creation and graphical representation of lottery data.&lt;/p&gt;&lt;p&gt;Copyright &amp;copy; 2021 &lt;a href=&quot;%2&quot; title=&quot;Visit organization&apos;s homepage&quot;&gt;%3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;This application is licensed under the terms of the &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.en.html&quot; title=&quot;Visit license&apos;s homepage&quot;&gt;GNU General Public License, version 3&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -39,7 +39,7 @@
     <name>ColophonCreditsPage</name>
     <message>
         <location filename="../colophon_credits_page.cpp" line="32"/>
-        <source>&lt;html&gt;&lt;body&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Freepik&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Application logo made by &lt;a href=&quot;https://www.flaticon.com/authors/freepik/&quot; title=&quot;Visit author&apos;s homepage&quot;&gt;Freepik&lt;/a&gt; from &lt;a href=&quot;https://www.flaticon.com&quot; title=&quot;Visit organization&apos;s homepage&quot;&gt;www.flaticon.com&lt;/a&gt; is licensed under &lt;a href=&quot;https://file000.flaticon.com/downloads/license/license.pdf&quot; title=&quot;Free License (with attribution)&quot;&gt;Free License (with attribution)&lt;/a&gt;.&lt;/dd&gt;&lt;dt&gt;&lt;strong&gt;BreezeIcons project&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Application icons made by &lt;a href=&quot;https://api.kde.org/frameworks/breeze-icons/html/&quot; title=&quot;Visit project&apos;s homepage&quot;&gt;BreezeIcons project&lt;/a&gt; from &lt;a href=&quot;https://kde.org&quot; title=&quot;Visit organization&apos;s homepage&quot;&gt;KDE&lt;/a&gt; are licensed under &lt;a href=&quot;https://www.gnu.org/licenses/lgpl-3.0.en.html&quot; title=&quot;GNU Lesser General Public License Version 3&quot;&gt;LGPLv3&lt;/a&gt;.&lt;/dd&gt;&lt;/dl&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <source>&lt;html&gt;&lt;body&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Freepik&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Application logo made by &lt;a href=&quot;https://www.flaticon.com/authors/freepik/&quot; title=&quot;Visit author&apos;s homepage&quot;&gt;Freepik&lt;/a&gt; from &lt;a href=&quot;https://www.flaticon.com&quot; title=&quot;Visit organization&apos;s homepage&quot;&gt;www.flaticon.com&lt;/a&gt; is licensed under &lt;a href=&quot;https://file000.flaticon.com/downloads/license/license.pdf&quot; title=&quot;Visit license&apos;s homepage&quot;&gt;Free License (with attribution)&lt;/a&gt;.&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;BreezeIcons project&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Application icons made by &lt;a href=&quot;https://api.kde.org/frameworks/breeze-icons/html/&quot; title=&quot;Visit project&apos;s homepage&quot;&gt;BreezeIcons project&lt;/a&gt; from &lt;a href=&quot;https://kde.org&quot; title=&quot;Visit organization&apos;s homepage&quot;&gt;KDE&lt;/a&gt; are licensed under &lt;a href=&quot;https://www.gnu.org/licenses/lgpl-3.0.en.html&quot; title=&quot;Visit license&apos;s homepage&quot;&gt;LGPLv3&lt;/a&gt;.&lt;/dd&gt;&lt;/dl&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -60,7 +60,7 @@
     <name>ColophonEnvironmentPage</name>
     <message>
         <location filename="../colophon_environment_page.cpp" line="39"/>
-        <source>&lt;html&gt;&lt;body&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Application version&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;%1&lt;/dd&gt;&lt;dt&gt;&lt;strong&gt;Qt for C++ version&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Qt %2 (Built against %3)&lt;/dd&gt;&lt;dt&gt;&lt;strong&gt;Operation System&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;%4 (Kernel %5 on %6)&lt;/dd&gt;&lt;/dl&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <source>&lt;html&gt;&lt;body&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Application version&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;%1&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Qt for C++ version&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;Qt %2 (Built against %3)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;&lt;strong&gt;Operation System&lt;/strong&gt;&lt;/dt&gt;&lt;dd&gt;%4 (Kernel %5 on %6)&lt;/dd&gt;&lt;/dl&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -73,7 +73,7 @@
     <name>ColophonLicensePage</name>
     <message>
         <location filename="../colophon_license_page.cpp" line="33"/>
-        <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;%1 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;/p&gt;&lt;p&gt;%1 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.&lt;/p&gt;&lt;p&gt;You should have received a copy of the GNU General Public License along with %1. If not, see &lt;a href=&quot;https://www.gnu.org/licenses/&quot;&gt;https://www.gnu.org/licenses/&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <source>&lt;html&gt;&lt;body&gt;&lt;p&gt;%1 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;/p&gt;&lt;p&gt;%1 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.&lt;/p&gt;&lt;p&gt;You should have received a copy of the GNU General Public License along with %1. If not, see &lt;a href=&quot;https://www.gnu.org/licenses/&quot; title=&quot;Visit license&apos;s homepage&quot;&gt;https://www.gnu.org/licenses/&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -98,168 +98,168 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../main_window.cpp" line="54"/>
+        <location filename="../main_window.cpp" line="142"/>
         <source>Eurojackpot</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="54"/>
+        <location filename="../main_window.cpp" line="142"/>
         <source>Eurojackpot is a transnational European lottery</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="55"/>
+        <location filename="../main_window.cpp" line="143"/>
         <source>EuroMillions</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="55"/>
+        <location filename="../main_window.cpp" line="143"/>
         <source>EuroMillions is a transnational European lottery</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="56"/>
+        <location filename="../main_window.cpp" line="144"/>
         <source>Vikinglotto</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="56"/>
+        <location filename="../main_window.cpp" line="144"/>
         <source>Vikinglotto is a transnational European lottery</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="63"/>
+        <location filename="../main_window.cpp" line="151"/>
         <source>About %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="66"/>
+        <location filename="../main_window.cpp" line="154"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="67"/>
+        <location filename="../main_window.cpp" line="155"/>
         <source>Brief description of the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="70"/>
+        <location filename="../main_window.cpp" line="158"/>
         <source>Colophon</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="72"/>
+        <location filename="../main_window.cpp" line="160"/>
         <source>Lengthy description of the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="75"/>
+        <location filename="../main_window.cpp" line="163"/>
         <source>Preferences…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="78"/>
+        <location filename="../main_window.cpp" line="166"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="79"/>
+        <location filename="../main_window.cpp" line="167"/>
         <source>Customize the appearance and behavior of the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="82"/>
-        <location filename="../main_window.cpp" line="85"/>
+        <location filename="../main_window.cpp" line="170"/>
+        <location filename="../main_window.cpp" line="173"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="87"/>
+        <location filename="../main_window.cpp" line="175"/>
         <source>Quit the application [%1]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="108"/>
+        <location filename="../main_window.cpp" line="196"/>
         <source>Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="113"/>
+        <location filename="../main_window.cpp" line="201"/>
         <source>Show Application Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="116"/>
+        <location filename="../main_window.cpp" line="204"/>
         <source>Display the Application toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="119"/>
+        <location filename="../main_window.cpp" line="207"/>
         <source>Show Lotteries Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="122"/>
+        <location filename="../main_window.cpp" line="210"/>
         <source>Display the Lotteries toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="125"/>
+        <location filename="../main_window.cpp" line="213"/>
         <source>Show View Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="128"/>
+        <location filename="../main_window.cpp" line="216"/>
         <source>Display the View toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="136"/>
+        <location filename="../main_window.cpp" line="224"/>
         <source>Full Screen Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="139"/>
+        <location filename="../main_window.cpp" line="227"/>
         <source>Display the window in full screen [%1]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="142"/>
+        <location filename="../main_window.cpp" line="230"/>
         <source>Exit Full Screen Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="145"/>
+        <location filename="../main_window.cpp" line="233"/>
         <source>Exit the full screen mode [%1]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="153"/>
+        <location filename="../main_window.cpp" line="241"/>
         <source>Application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="163"/>
+        <location filename="../main_window.cpp" line="251"/>
         <source>Lotteries</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="168"/>
+        <location filename="../main_window.cpp" line="256"/>
         <source>View</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="181"/>
+        <location filename="../main_window.cpp" line="269"/>
         <source>Application Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="190"/>
+        <location filename="../main_window.cpp" line="278"/>
         <source>Lotteries Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main_window.cpp" line="196"/>
+        <location filename="../main_window.cpp" line="284"/>
         <source>View Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
@@ -269,6 +269,19 @@
     <message>
         <location filename="../preferences_dialog.cpp" line="31"/>
         <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDrawsPage</name>
+    <message>
+        <location filename="../preferences_draws_page.cpp" line="29"/>
+        <source>&lt;strong style=&quot;font-size:large;&quot;&gt;Draws&lt;/strong&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferences_draws_page.cpp" line="47"/>
+        <source>Draws</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -302,6 +315,32 @@
     <message>
         <location filename="../preferences_general_page.cpp" line="66"/>
         <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesLotteriesPage</name>
+    <message>
+        <location filename="../preferences_lotteries_page.cpp" line="29"/>
+        <source>&lt;strong style=&quot;font-size:large;&quot;&gt;Lotteries&lt;/strong&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferences_lotteries_page.cpp" line="47"/>
+        <source>Lotteries</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesPlaysPage</name>
+    <message>
+        <location filename="../preferences_plays_page.cpp" line="29"/>
+        <source>&lt;strong style=&quot;font-size:large;&quot;&gt;Plays&lt;/strong&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferences_plays_page.cpp" line="47"/>
+        <source>Plays</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
